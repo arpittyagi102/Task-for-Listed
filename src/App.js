cee065cd9bc04eef0756e829fbfa5bb981320d3b
Arpit <arpittyagirocks@gmail.com>
2023-04-15 16:51:43 +0530

    created side-bar for the first screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import SignInPage from './components/Sign-In/Sign-In-page';
 
 function App() {
   return (
     <>
+      <SignInPage/>
       <h1>This is a fresh new React App</h1>
     </>
   );
---
+++ src/components/Sign-In/Sign-In-page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './SignInPage.css';
+
+export default function SignInPage(){
+    return(
+        <>
+        <div className="side-board">
+            <h1 className="side-board-heading">Board.</h1>
+        </div>
+        
+        </>
+    )
+}
