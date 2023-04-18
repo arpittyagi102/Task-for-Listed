f2f4e7276a66ad39a8b3b34dd74dd9de1095e1ac
Arpit <arpittyagirocks@gmail.com>
2023-04-18 14:21:57 +0530

    Created the head part of the Dashboard

---
+++ src/components/Dashboard/Colorbox.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './dashboard.css';
+
+export default function Colorbox(){
+    return(
+        <>
+            
+        </>
+    )
+}
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,6 +3,9 @@ import "./dashboard.css";
 //import MyChart from './MyChart'
 import Dashboardsidebar from "./Dashboard-sidebar/Dashboardsidebar";
 //import LineChart from "./LineChart";
+import searchicon from './search-icon.png'
+import bell from './bell.png'
+import dp from './dp.png'
 
 export default function Dashboard() {
     return (
@@ -14,9 +17,18 @@ export default function Dashboard() {
                 <div className="main-page">
                     <div className="header">
                         Dashboard
-                    <div className="search-outer">
-                        search icon image
+                        <div className="header-right">
+                            <div className="search-outer">
+                                <input className="search-bar" placeholder="Search..." />
+                                <img src={searchicon} alt="search icon" className="search-icon" />
+                            </div>
+
+                            <img src={bell} alt="bell-icon" className="bell" />
+                            <img src={dp} alt="DP" className="dp" />
+                        </div>
                     </div>
+                    <div className="color-boxes">
+                        
                     </div>
                 </div>
             </div>
