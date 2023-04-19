1456b6c27dc1719ea6897132428270b284758389
Arpit <arpittyagirocks@gmail.com>
2023-04-19 15:23:22 +0530

    customized google sign in button and responsive sign in page

--- src/components/Sign-In/Sign-In-page.js
+++ src/components/Sign-In/Sign-In-page.js
@@ -1,19 +1,28 @@
 import React from "react";
 import './SignInPage.css';
 import appleicon from './apple-icon.svg';
-import { GoogleLogin } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
+import googleicon from './google-icon.svg'
 import jwtDecode from "jwt-decode";
 import { Link } from "react-router-dom";
 
 var userdata;
 export default function SignInPage() {
 
-    function loggednin(response) {
-        userdata=jwtDecode(response.credential);
-        console.log("Name : "+userdata.name)    
-        console.log("email : "+userdata.email)    
-        console.log("Picture : "+userdata.picture);
-        window.location.href="/dashboard";    
+    const login = useGoogleLogin({
+        onSuccess: codeResponse => console.log(codeResponse),
+        flow: 'auth-code',
+        redirect_uri:"https://taskforlisted.netlify.app/dashboard",
+      });
+    
+    function loggedin(response) {
+        console.log(response);
+        //userdata = jwtDecode(response);
+        /* console.log(userdata);
+        console.log("Name : " + userdata.name)
+        console.log("email : " + userdata.email)
+        console.log("Picture : " + userdata.picture);
+        window.location.href="/dashboard";     */
     }
 
     return (
@@ -25,15 +34,17 @@ export default function SignInPage() {
                 <div className="sign-in-outer">
                     <p className="sign-in-heading">Sign In</p>
                     <p style={{ fontFamily: 'Lato', margin: "5px" }}>Sign into your account</p>
-                    <div classname="button-cover" style={{ display: "flex", justifyContent: "space-between", margin: "25px 0px" }}>
+                    <div className="button-cover">
 
                         {/* <img src={googleicon} alt="Not loading" />
                             Sign In with Google */}
-                        <GoogleLogin onSuccess={loggednin} />
-
-                        <div className="white-buttons">
+                        <div className="white-buttons" id="b1" onClick={() => login()}>
+                            <img src={googleicon} alt="Not loading" />
+                            <p>Sign In with Apple</p>
+                        </div>
+                        <div className="white-buttons" id="b2">
                             <img src={appleicon} alt="Not loading" />
-                            Sign In with Apple
+                            <p>Sign In with Apple</p>
                         </div>
                     </div>
                     <div className="form-outer">
@@ -42,7 +53,7 @@ export default function SignInPage() {
                         <label>Password</label>
                         <input className="input" type="password" />
                         <p className="forgot">Forgot password?</p>
-                        <Link to={'./dashboard'} style={{"textDecoration":"none"}}>
+                        <Link to={'./dashboard'} style={{ "textDecoration": "none" }}>
                             <div className="main-sign-in-button">Sign In</div>
                         </Link>
                     </div>
